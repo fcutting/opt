62db66a488f0d554dfc95148ae12c0052a2b4b54
FletcherCutting <admin@fletchercutting.au>
2024-03-21 19:50:44 +1100

    Add Option type

--- opt.go
+++ opt.go
@@ -1,3 +1,13 @@
 // Package opt provides a generic Option type that holds a value of a provided
 // type and a boolean flag indicating whether the value was provided.
 package opt
+
+// Option represents a generic type that holds a value of any type T and a
+// boolean flag indication whether the value was provided.
+type Option[T any] struct {
+	// value holds the value of type T.
+	value T
+
+	// exists indicates whether the value was provided.
+	exists bool
+}
