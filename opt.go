4fe28dde0275bbc7a368392d853cdbdc06f28432
FletcherCutting <admin@fletchercutting.au>
2024-03-23 10:44:23 +1100

    Add MarshalJSON UnmarshalJSON functions

--- opt.go
+++ opt.go
@@ -2,6 +2,16 @@
 // type and a boolean flag indicating whether the value was provided.
 package opt
 
+import (
+	"encoding/json"
+	"reflect"
+)
+
+// Represents the JSON null string in bytes.
+// This isn't a constant because Go doesn't allow for converted types to be
+// constants.
+var nullBytes = []byte("null")
+
 // Option represents a generic type that holds a value of any type T and a
 // boolean flag indication whether the value was provided.
 type Option[T any] struct {
@@ -10,4 +20,32 @@ type Option[T any] struct {
 
 	// exists indicates whether the value was provided.
 	exists bool
+}
+
+// MarshalJSON converts the Option value to its JSON representation.
+// If the value is not provided, MarshalJSON will return "null".
+func (o Option[T]) MarshalJSON() (data []byte, err error) {
+	if o.exists {
+		return json.Marshal(o.value)
+	}
+
+	return nullBytes, nil
+}
+
+// UnmarshalJSON parses the JSON-encoded data and stores the result in the
+// Option value.
+func (o *Option[T]) UnmarshalJSON(data []byte) (err error) {
+	if reflect.DeepEqual(data, nullBytes) {
+		return nil
+	}
+
+	// I check if the Unmarshal works first before setting exists to true because
+	// if the Unmarshal fails and the caller continues despite the error then
+	// exists being true is incorrect
+	if err = json.Unmarshal(data, &o.value); err != nil {
+		return
+	}
+
+	o.exists = true
+	return nil
 }
---
+++ opt_test.go
@@ -0,0 +1,156 @@
+package opt_test
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/fcutting/opt"
+	"github.com/gkampitakis/go-snaps/snaps"
+)
+
+func TestMain(m *testing.M) {
+	r := m.Run()
+	snaps.Clean(m, snaps.CleanOpts{Sort: true})
+	os.Exit(r)
+}
+
+type testPayload struct {
+	Primitive *opt.Option[string]         `json:"primitive,omitemty"`
+	Map       *opt.Option[map[string]any] `json:"map,omitempty"`
+	Struct    *opt.Option[struct {
+		Make  string
+		Model string
+	}] `json:"struct,omitempty"`
+	Slice *opt.Option[[]int] `json:"slice,omitempty"`
+}
+
+type testCase struct {
+	data []byte
+}
+
+var testCases = map[string]testCase{
+	"Empty": {
+		data: []byte(`
+			{
+				"empty": true
+			}
+		`),
+	},
+	"Primitive empty": {
+		data: []byte(`
+			{
+				"primitive": ""
+			}
+		`),
+	},
+	"Primitive": {
+		data: []byte(`
+			{
+				"primitive": "hello world"
+			}
+		`),
+	},
+	"Primitive null": {
+		data: []byte(`
+			{
+				"primitive": null
+			}
+		`),
+	},
+	"Map empty": {
+		data: []byte(`
+			{
+				"map": {}
+			}
+		`),
+	},
+	"Map full": {
+		data: []byte(`
+			{
+				"map": {
+					"make": "Toyota",
+					"model": "Hilux"
+				}
+			}
+		`),
+	},
+	"Map null": {
+		data: []byte(`
+			{
+				"map": {}
+			}
+		`),
+	},
+	"Struct empty": {
+		data: []byte(`
+			{
+				"struct": {}
+			}
+		`),
+	},
+	"Struct full": {
+		data: []byte(`
+			{
+				"struct": {
+					"make": "Toyota",
+					"model": "Hilux"
+				}
+			}
+		`),
+	},
+	"Struct null": {
+		data: []byte(`
+			{
+				"struct": null
+			}
+		`),
+	},
+	"Slice empty": {
+		data: []byte(`
+			{
+				"slice": []
+			}
+		`),
+	},
+	"Slice full": {
+		data: []byte(`
+			{
+				"slice": [1, 2, 3]
+			}
+		`),
+	},
+	"Slice null": {
+		data: []byte(`
+			{
+				"slice": null
+			}
+		`),
+	},
+}
+
+func Test_Option(t *testing.T) {
+	for n, c := range testCases {
+		t.Run(n, func(t *testing.T) {
+			var payload testPayload
+
+			if err := json.Unmarshal(c.data, &payload); err != nil {
+				t.Fatalf("Unexpected unmarshal error: %s", err)
+			}
+
+			t.Run("MarshalJSON", func(t *testing.T) {
+				test_Marshal(t, payload)
+			})
+		})
+	}
+}
+
+func test_Marshal(t *testing.T, payload testPayload) {
+	result, err := json.Marshal(payload)
+
+	if err != nil {
+		t.Fatalf("Unexpected marshal error: %s", err)
+	}
+
+	snaps.MatchSnapshot(t, string(result))
+}
